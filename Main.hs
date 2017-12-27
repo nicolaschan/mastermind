33404a55667de2a52b6872ebaca038f26e81ff17
Nicolas Chan <nicolas@nicolaschan.com>
2017-12-26 17:22:02 -0800

    Prompt

--- Main.hs
+++ Main.hs
@@ -1,17 +1,33 @@
 import Control.Parallel
 import Control.Parallel.Strategies
 import Mastermind
+import System.IO
+
+prompt :: String -> IO String
+prompt s = do
+  putStr s
+  hFlush stdout
+  getLine
+
+guessAndPrompt :: [Result] -> IO ()
+guessAndPrompt results = do
+    let guess = nextGuess 4 [1..6] results
+    print guess
+    correct <- prompt "Correct: " 
+    incorrect <- prompt "Incorrect: " 
+    guessAndPrompt ((guess,(read correct, read incorrect)):results)
 
 main :: IO ()
-main = do
+main = guessAndPrompt [] 
     -- 4152
     -- avg     print $ nextGuess 4 [1..6] [([1,1,2,3],(1,1)),([1,4,1,5],(0,3)),([4,2,6,1],(1,2))] 
     -- median  print $ nextGuess 4 [1..6] [([1,2,3,4],(0,3)),([2,3,4,5],(0,3)),([3,1,5,2],(3,0))]
     -- mode    print $ nextGuess 4 [1..6] [([1,1,2,2],(2,0)),([1,1,3,4],(1,1)),([1,3,2,5],(0,3))]
     -- minimum print $ nextGuess 4 [1..6] [([1,2,3,4],(0,3)),([2,1,4,5],(1,3)),([2,4,5,1],(1,3))]
     --print $ nextGuess 4 [1..6] []
-    let result = parMap rdeepseq (simulate 4 [1..6]) space 
-    print result
-    print (avg $ map fromIntegral result)
+    --let result = parMap rdeepseq (simulate 4 [1..6]) space 
+    --print result
+    --print (avg $ map fromIntegral result)
     --print $ nextGuess 4 [1..6] [([1,1,2,3],(2,0)),([1,4,2,5],(1,0)),([2,6,2,3],(2,0)),([1,6,3,3],(1,1)),([3,3,2,3],(3,0))] 
     --print $ nextGuess 4 [1..6] [([1,1,2,2],(0,1)),([2,3,4,4],(0,1)),([3,5,1,6],(1,2)),([1,4,6,1],(2,1))]
+    --print $ nextGuess 4 [1..6] [([1,1,2,2],(2,0)),([2,3,4,4],(0,2)),([3,3,1,5],(0,3)),([2,3,1,5],(0,4)),([1,2,2,3],(2,1))]
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# mastermind
+# Mastermind
 💡 Mastermind solver
+
+## Usage
+Run with `cabal run` and it will give a guess and prompt you for the score. The guess uses the numbers `[1..6]` to represent the six colors. By symmetry, it does not matter which color corresponds to which number as long as you are consistent.
